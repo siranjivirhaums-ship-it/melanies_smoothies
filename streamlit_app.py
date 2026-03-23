c2cd665154837ef92e5f96b1a00d018f6798cffc
siranjivirhaums-ship-it <siranjivirhaum.s@gmail.com>
2026-03-23 16:15:37 +0530

    Update streamlit_app.py
    moved import and removed lines, changes line to for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 # Write directly to the app
 st.title(f"🥤 Customize Your Smoothie! 🥤")
 st.write(
@@ -30,7 +30,10 @@ if ingredients_list:
     ingredients_string =''
 
     for each_fruit in ingredients_list:
+      
         ingredients_string += each_fruit+' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
     #st.write(ingredients_string)
 
@@ -45,7 +48,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!' ,icon="✅")
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+  
+
